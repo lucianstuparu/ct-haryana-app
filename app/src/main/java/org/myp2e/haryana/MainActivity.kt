0b8a4a56a0f61b36711feb9d651e60afeda0941e
Lucian Stuparu <lucianstuparu@gmail.com>
2024-05-28 14:50:40 +0200

    Initial setup for Haryana app webview in an empty Android app

---
+++ app/src/main/java/org/myp2e/haryana/MainActivity.kt
@@ -0,0 +1,41 @@
+package org.myp2e.haryana
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.google.accompanist.web.WebView
+import com.google.accompanist.web.rememberWebViewState
+import org.myp2e.haryana.ui.theme.HaryanaP2ETheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            HaryanaP2ETheme {
+                WebViewScreen("https://haryana.myp2e.org/home/defaultsignin?whr=uri%3aB2C&referrer=basedomain&domain_hint=haryana.myp2e.org")
+            }
+        }
+    }
+}
+
+@Composable
+fun WebViewScreen(url: String) {
+    val state = rememberWebViewState(url)
+
+    WebView(
+        state = state,
+        modifier = Modifier.fillMaxSize(),
+        onCreated = { webView ->
+            webView.settings.javaScriptEnabled = true
+        }
+    )
+}
